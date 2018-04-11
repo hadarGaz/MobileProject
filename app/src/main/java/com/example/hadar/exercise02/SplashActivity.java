910fb34d2d8cbce96daf8749638e199db4dfeaae
Ofir Uzan - Laptop <ophiruzan3@gmail.com>
2018-04-11 19:09:15 +0300

    User Details Activity updated

--- app/src/main/java/com/example/hadar/exercise02/SplashActivity.java
+++ app/src/main/java/com/example/hadar/exercise02/SplashActivity.java
@@ -9,22 +9,22 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class SplashActivity extends AppCompatActivity
 {
+    GoogleSignInAccount m_googleAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Thread myThread = new Thread()
-        {
+        Thread myThread = new Thread() {
             @Override
             public void run()
             {
                 try
                 {
                     sleep(3000);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
+                    moveToNextActivity();
                     finish();
                 }
 
@@ -38,28 +38,29 @@ public class SplashActivity extends AppCompatActivity
         myThread.start();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
-    }
-
-    private void updateUI(GoogleSignInAccount account)
+    public void moveToNextActivity()
     {
         Intent nextActivityIntent;
 
-        if(account!=null) {
+        //Add || m_facebookAccount != null || m_emailPasswordAccount != null ...
+        if (m_googleAccount != null)
+        {
             nextActivityIntent = new Intent(getApplicationContext(), UserDetailsActivity.class);
-
+            nextActivityIntent.putExtra("Google Account", m_googleAccount);
         }
-        else {
+        else
             nextActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
-        }
 
         startActivity(nextActivityIntent);
+    }
 
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        m_googleAccount = GoogleSignIn.getLastSignedInAccount(this);
     }
 }
--- app/src/main/java/com/example/hadar/exercise02/UserDetailsActivity.java
+++ app/src/main/java/com/example/hadar/exercise02/UserDetailsActivity.java
@@ -1,13 +1,44 @@
 package com.example.hadar.exercise02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class UserDetailsActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+public class UserDetailsActivity extends AppCompatActivity
+{
+    String m_userName, m_userEmail;
+    ImageView m_userPhoto;
+    Button m_signOutButton;
+    TextView m_TextViewUserName, m_TextViewUserEmail;
+    Intent m_inputIntent;
+    GoogleSignInAccount m_googleAccount;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_details2);
+        setContentView(R.layout.activity_user_details);
+
+        findViews();
+        getIntentInput();
+    }
+
+    private void getIntentInput()
+    {
+        m_inputIntent = getIntent();
+        m_googleAccount = m_inputIntent.getParcelableExtra("Google Account");
+    }
+
+
+    public void findViews()
+    {
+        m_TextViewUserName = findViewById(R.id.user_name);
+        m_TextViewUserEmail = findViewById(R.id.user_email);
+        m_signOutButton = findViewById(R.id.sign_out_button);
     }
 }
