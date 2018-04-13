cfa1e8483763df1e6985e4879a38eb9ccde84bb8
Liad Pasker <liadpasker@gmail.com>
2018-04-13 23:11:09 +0300

    slide_out_right.xml animation added

--- app/src/main/java/com/example/hadar/exercise02/MainActivity.java
+++ app/src/main/java/com/example/hadar/exercise02/MainActivity.java
@@ -168,14 +168,8 @@ public class MainActivity extends Activity
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-          //                  Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = m_firebaseAuth.getCurrentUser();
                         //    updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-            //                Log.w(TAG, "signInWithCredential:failure", task.getException());
-              //              Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                            updateUI(null);
                         }
                     }
                 });
