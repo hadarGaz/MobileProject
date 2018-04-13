04fef2b70aa106b841680b374502a8d75b561cd0
Liad Pasker <liadpasker@gmail.com>
2018-04-13 15:58:14 +0300

    google loading bar added app theme updated animation between activities was added

--- app/src/main/java/com/example/hadar/exercise02/MainActivity.java
+++ app/src/main/java/com/example/hadar/exercise02/MainActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Toast;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -13,6 +17,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import pl.droidsonroids.gif.GifTextView;
 
 public class MainActivity extends Activity
 {
@@ -20,6 +25,8 @@ public class MainActivity extends Activity
     private FirebaseAuth m_firebaseAuth;
     private GoogleSignInClient m_googleSignInClient;
     private UserDetails m_userDetails;
+    private GifTextView m_googleLoadingBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,6 +34,7 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         m_firebaseAuth = FirebaseAuth.getInstance();
+        m_googleLoadingBar=findViewById(R.id.google_load_bar);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -72,8 +80,10 @@ public class MainActivity extends Activity
     public void onClickGoogleButton()
     {
         Intent signInIntent = m_googleSignInClient.getSignInIntent();
+        playGif();
         startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
     }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -114,6 +124,33 @@ public class MainActivity extends Activity
             m_userDetails = new UserDetails(i_GoogleSignInAccount);
             userDetails.putExtra("User Details", m_userDetails);
             startActivity(userDetails);
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         }
     }
+
+
+    public void playGif(){
+        final Animation googleLoader = new AlphaAnimation(1.f, 1.f);
+        //googleLoader.setDuration(20000);
+        googleLoader.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation)
+            {
+
+                m_googleLoadingBar.setBackgroundResource(R.drawable.google_dark_load);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation){}
+
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                m_googleLoadingBar.clearAnimation();
+            }
+        });
+        m_googleLoadingBar.startAnimation(googleLoader);
+    }
+
 }
