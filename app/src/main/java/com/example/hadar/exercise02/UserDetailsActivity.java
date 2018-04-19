0e28fd7e3626e426bc105c7da7ab3298dcab8a4e
hadar <shctr_adr@walla.com>
2018-04-19 21:10:30 +0300

    design

--- app/src/main/java/com/example/hadar/exercise02/UserDetailsActivity.java
+++ app/src/main/java/com/example/hadar/exercise02/UserDetailsActivity.java
@@ -2,13 +2,11 @@ package com.example.hadar.exercise02;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
@@ -121,10 +119,10 @@ public class UserDetailsActivity extends AppCompatActivity
 
     private void findViews()
     {
-        m_UserNameTextView = findViewById(R.id.user_name);
-        m_UserEmailTextView = findViewById(R.id.user_email);
-        m_signOutButton = findViewById(R.id.sign_out_button);
-        m_userPictureImageView = findViewById(R.id.user_picture);
+        m_UserNameTextView = findViewById(R.id.TextViewUserName);
+        m_UserEmailTextView = findViewById(R.id.TextViewEmail);
+        m_signOutButton = findViewById(R.id.buttonSignOut);
+        m_userPictureImageView = findViewById(R.id.ImageViewUserPic);
     }
 
     private void signOutAllAccounts()
