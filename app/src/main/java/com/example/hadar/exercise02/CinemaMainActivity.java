77df5f3087615d3538e71f67d9a259ca1bb61e26
Liad Pasker <liadpasker@gmail.com>
2018-04-25 13:49:16 +0300

    update

--- app/src/main/java/com/example/hadar/exercise02/CinemaMainActivity.java
+++ app/src/main/java/com/example/hadar/exercise02/CinemaMainActivity.java
@@ -3,18 +3,48 @@ package com.example.hadar.exercise02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class CinemaMainActivity extends AppCompatActivity {
+public class CinemaMainActivity extends AppCompatActivity
+{
 
     RecyclerView m_recyclerView;
     UserDetails m_userDetails;
-    ImageButton m_profileMenu;
+    ImageButton m_profileMenuButton;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cinema_main);
+
+        m_profileMenuButton=findViewById(R.id.profileMenuButton);
+
+
+}
+
+
+    private void onClickProfileButtom
+    {
+        PopupMenu popup = new PopupMenu(this, m_profileMenuButton);
+        popup.getMenuInflater().inflate(R.menu.activity_cinema_main, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+        {
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                Toast.makeText(CinemaMainActivity.this, "You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        popup.show();
     }
+
+
 }
